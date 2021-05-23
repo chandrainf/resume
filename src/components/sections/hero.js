9077bee151396cb16793be8873eee8f584d0b029
chandrainf <surel.chandrapratama@gmail.com>
2021-05-23 16:24:07 +0700

    update

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,14 +59,14 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
+  const two = <h2 className="big-heading">Chandra Pratama Putra.</h2>;
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <p>
-      I'm a Boston-based software engineer who specializes in building (and occasionally designing)
-      exceptional digital experiences. Currently, I'm an engineer at{' '}
-      <a href="https://upstatement.com/">Upstatement</a> focused on building accessible,
-      human-centered products.
+      I'm a Bogor-based front-end enthusiast who specializes in building (and occasionally designing)
+      exceptional digital experiences. Currently, I'm an office administrator at{' '}
+      <a href="https://pancaduta.com/">Upstatement</a> focused on administrative,
+      tax preparer.
     </p>
   );
   const five = (
