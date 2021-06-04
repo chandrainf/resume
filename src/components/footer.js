3013f43ccaae96e83e3fb1d5b69084c9d995b80b
chandrainf <surel.chandrapratama@gmail.com>
2021-06-04 11:28:31 +0700

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -106,7 +106,7 @@ const Footer = () => {
 
       <StyledCredit tabindex="-1">
         <a href="https://github.com/chandrainf/resume">
-          <div>Designed &amp; Built by Brittany Chiang</div>
+          <div>Chandra Pratama Putra &amp; 2021</div>
 
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
