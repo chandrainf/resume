08f41f87462c6367d990d4ad66ae24141127467e
chandrainf <surel.chandrapratama@gmail.com>
2021-06-05 16:13:09 +0700

    loader

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-export const hex2rgba = (hex, alpha = 1) => {
+export const hex2rgba = (hex, alpha = 2) => {
   const [r, g, b] = hex.match(/\w\w/g).map(x => parseInt(x, 16));
   return `rgba(${r},${g},${b},${alpha})`;
 };
